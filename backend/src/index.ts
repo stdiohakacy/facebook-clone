2b71cfec9ddf611e92a7742e1abebb0d508afa0f
Cornal <nguyendangduy2210@gmail.com>
2022-08-16 18:54:38 +0700

    add pgadmin service

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,20 +30,3 @@ PostgresDataSource.initialize()
 }).catch((err) => {
     console.error("Error during Data Source initialization", err)
 })
-
-//Connects to the Database -> then starts the express
-// createConnection()
-//   .then(async connection => {
-//     // Create a new express application instance
-//     const app = express();
-//     // Call midlewares
-//     app.use(cors());
-//     app.use(helmet());
-//     app.use(bodyParser.json());
-//     //Set all routes from routes folder
-
-//     app.listen(3000, () => {
-//       console.log("Server started on port 3000!");
-//     });
-//   })
-//   .catch(error => console.log(error));
