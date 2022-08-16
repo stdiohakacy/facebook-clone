81d91f070c85103dfc3ef57e931af75beba75982
Cornal <nguyendangduy2210@gmail.com>
2022-08-16 23:48:03 +0700

    init migration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,23 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { createConnection } from "typeorm";
 import express from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import cors from "cors";
 
-const PostgresDataSource = new DataSource({
-  type: "postgres",
-  host: "fb-postgres",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
-})
-
-PostgresDataSource.initialize()
-.then(() => {
-    console.log("Data Source has been initialized!")
+createConnection()
+  .then(async connection => {
     // Create a new express application instance
     const app = express();
-    // Call middle wares
+    // Call midlewares
     app.use(cors());
     app.use(helmet());
     app.use(bodyParser.json());
+    //Set all routes from routes folder
+
     app.listen(3000, () => {
-        console.log("Server started on port 3000!");
+      console.log("Server started on port 3000!");
+      console.log(connection)
     });
-    app.get("/", (req, res) => { return res.json({ message: "ABC" }) })
-}).catch((err) => {
-    console.error("Error during Data Source initialization", err)
-})
+  })
+  .catch(error => console.log(error));
