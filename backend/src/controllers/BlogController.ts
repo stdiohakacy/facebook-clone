ccf0528a5353030fde63097c03642608d5cf68e4
Cornal <nguyendangduy2210@gmail.com>
2022-08-17 01:31:02 +0700

    crud api blog

--- backend/src/controllers/BlogController.ts
+++ backend/src/controllers/BlogController.ts
@@ -1,6 +1,6 @@
 import { Body, Delete, Get, JsonController, Param, Post, Put, Req, Res } from "routing-controllers";
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getRepository, createQueryBuilder } from 'typeorm';
 import { Blog } from "../entity/Blog";
 
 @JsonController()
@@ -13,22 +13,43 @@ export class BlogController {
   }
 
   @Get('/blogs/:id')
-  getPostById(@Param('id') id: string) {
-    return 'This action returns post #' + id;
+  async getPostById(@Param('id') id: string, @Req() req: Request, @Res() res: Response) {
+    const blogRepo = await getRepository(Blog);
+    const blog = await blogRepo.findOne(id);
+
+    return res.json({ blog })
   }
 
   @Post('/blogs')
-  createPost(@Body() post: any) {
-    return 'Saving post...';
+  async createPost(@Body() post: any, @Req() req: Request, @Res() res: Response) {
+    const result = await createQueryBuilder()
+      .insert()
+      .into(Blog)
+      .values(post)
+      .execute()
+    
+    return res.json({ result })
   }
 
   @Put('/blogs/:id')
-  updatePost(@Param('id') id: number, @Body() post: any) {
-    return 'Updating a post...';
+  async updatePost(@Param('id') id: string, @Body() post: any, @Req() req: Request, @Res() res: Response) {
+    const result = await createQueryBuilder()
+      .update(Blog)
+      .set(post)
+      .where("id = :id", { id })
+      .execute()
+
+    return res.json({ result })
   }
 
   @Delete('/blogs/:id')
-  removePost(@Param('id') id: number) {
-    return 'Removing post...';
+  async removePost(@Param('id') id: string, @Body() post: any, @Req() req: Request, @Res() res: Response) {
+    const result = await createQueryBuilder()
+    .delete()
+    .from(Blog)
+    .where("id = :id", { id })
+    .execute()
+
+    return res.json({ result })
   }
 }
